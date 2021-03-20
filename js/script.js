37778e44c8067610d90879eefc61dcdaff668f4f
Qidsen <Yaros1406@gmail.com>
2021-03-20 21:40:31 +0200

    race00 yvovnenko

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 let input = document.querySelector('.input')
+let history = document.querySelector('.history')
+let counter = 0  // для локалстореджа
 
 function insert(num) {
-	if(input.textContent == 0) {
+	if(num === '.'){
+		let number = input.textContent
+		for (let i = number.length; i >= 0; i--){
+			if(number[i] == '+' || number[i] == '-' || number[i] == '*' || number[i] == '/')
+				number = number.slice(i)
+		}
+		if(number.includes('.'))
+			return
+
+		if(input.textContent == 0){
+			input.textContent = "0."
+			return
+		}
+	}
+	if(input.textContent == 0 || input.textContent == 'Infinity' || input.textContent == '-Infinity') {
 		input.textContent = ""
 		input.textContent = input.textContent + num
-	} else
-		input.textContent = input.textContent + num
+	} else{
+		if(input.textContent.slice(-1) == num && Number(isNaN(input.textContent.slice(-1))))
+			return
+		else input.textContent = input.textContent + num
+	}
 }
 
 function clean() {
@@ -19,28 +38,30 @@ function back() {
 		input.textContent = "0"
 }
 
-function equal() {
-	let exp = input.textContent
-	if(exp)
-		input.textContent = eval(exp)
+function equal() { 
+    history.textContent = input.textContent 
+    localStorage.setItem("id" + counter, history.textContent)
+    counter++
+    let exp = input.textContent 
+    if (exp) { 
+        input.textContent = eval(exp) 
+        localStorage.setItem("id" + counter, input.textContent) 
+        counter++
+    } 
 }
 
 function percent() {
+	history.textContent = input.textContent
 	input.textContent = eval(input.textContent) / 100
 }
 
 function constant(name) {
-	if(name == "pi") {
-		input.textContent = ""
-		input.textContent = input.textContent + Math.PI.toFixed(8)
-	}
-	if(name == "e") {
-		input.textContent = ""
-		input.textContent = input.textContent + Math.E.toFixed(8)
-	}
+	if(name == "pi") input.textContent = input.textContent + Math.PI.toFixed(8)
+	if(name == "e") input.textContent = input.textContent + Math.E.toFixed(8)
 }
 
 function operation(name) {
+	history.textContent = name + '(' + input.textContent + ')'
 	if(name == "sqrt")
 		input.textContent = Math.sqrt(eval(input.textContent))
 	if(name == "sqr")
@@ -50,6 +71,7 @@ function operation(name) {
 }
 
 function factorial(n) {
+	history.textContent =  input.textContent + '!'
 	return (n != 1) ? n * factorial(n - 1) : 1
 }
 
@@ -57,11 +79,9 @@ function fact() {
 	input.textContent = factorial(+eval(input.textContent))
 }
 
-function log(name) {
-	if(name == 'lg')
-		input.textContent = Math.log10(eval(input.textContent)).toFixed(8)
-	if(name == 'ln')
-		input.textContent = Math.log(eval(input.textContent)).toFixed(8)
+function log() {
+	history.textContent = 'lg(' + input.textContent + ')'
+	input.textContent = Math.log10(eval(input.textContent)).toFixed(8)
 }
 
 document.querySelector('.type').addEventListener('click', function() {
@@ -100,4 +120,112 @@ function f(name) {
 
 function invert() {
 	input.textContent = -eval(input.textContent)
+}
+
+//for memory
+
+let memory
+let clipboard = []
+
+function ms() {
+    memory = parseFloat(input.textContent)
+    return memory
+}
+
+function mr() {
+	input.textContent = memory
+}
+
+function mc() {
+    return memory = "0"
+}
+
+function madd() {
+    const current = parseFloat(input.textContent)
+    if (isNaN(memory) || memory == "") {
+        console.error("Memory is empty")
+    } else {
+        history.textContent = current + "+" + memory
+        let x = memory + current
+        input.textContent = x
+        clipboard.push(history.textContent)
+        clipboard.push("= " + input.textContent + "\n")
+    }
+}
+
+function msubtract() {
+    const current = parseFloat(input.textContent)
+    if (isNaN(memory) || memory == "") {
+        console.error("Memory is empty")
+    } else {
+        history.textContent = current + "-" + memory
+        let x = current - memory
+        input.textContent = x
+        clipboard.push(history.textContent)
+        clipboard.push("= " + input.textContent + "\n")
+    }
+}
+
+function conventer(){
+	console.log(type)
+	let items = document.querySelectorAll("div.item")
+
+	for(let i of items){
+		i.style = 'display: none'
+	}
+}
+
+function weightConverterP(valNum) {
+    document.getElementById("Grams").value = valNum / 0.0022046;
+    document.getElementById("Kilograms").value = valNum / 2.2046;
+    document.getElementById("Ton").value = valNum * 0.00045359237;
+
+}
+
+function weightConverterG(valNum) {
+    document.getElementById("Pounds").value = valNum * 0.0022046;
+    document.getElementById("Kilograms").value = valNum / 1000;
+
+}
+
+function weightConverterK(valNum) {
+    document.getElementById("Pounds").value = valNum / 0.45359237;
+    document.getElementById("Grams").value = valNum * 1000;
+    document.getElementById("Ton").value = valNum / 1000;
+}
+
+function weightConverterT(valNum) {
+    document.getElementById("Pounds").value = valNum / 0.00045359237;
+    document.getElementById("Kilograms").value = valNum * 1000;
+}
+
+function lengthConverterC(valNum) {
+    document.getElementById("Meters").value = valNum / 100;
+    document.getElementById("Kilometers").value = valNum / 100000;
+}
+
+function lengthConverterM(valNum) {
+    document.getElementById("cm").value = valNum / 0.01;
+    document.getElementById("Kilometers").value = valNum / 1000;
+}
+
+function lengthConverterK(valNum) {
+    document.getElementById("cm").value = valNum * 100000;
+    document.getElementById("Meters").value = valNum * 1000;
+}
+
+function areaConverterK(valNum) {
+    document.getElementById("m").value = valNum * 1000000;
+    document.getElementById("acres").value = valNum * 247.105381;
+}
+
+function areaConverterA(valNum) {
+    document.getElementById("km").value = valNum / 247.105381;
+    document.getElementById("m").value = valNum / 0.00024711;
+
+}
+	
+function areaConverterM(valNum) {
+    document.getElementById("acres").value = valNum * 0.000247;
+    document.getElementById("km").value = valNum / 1000000;
 }
